67cae1ad47c7cd76e65e1317890a4e1e81f40770
mdjahidulhamid <jahidulhamid@yahoo.com>
2016-01-09 23:07:58 +0600

    4.0.2

--- install.sh
+++ install.sh
@@ -10,4 +10,5 @@ mkdir -p /usr/local/oraji &&
 cp -fr ./* /usr/local/oraji &&
 chmod -R 755 /usr/local/oraji &&
 ln -sf /usr/local/oraji/oraji /usr/bin/oraji &&
+ln -sf /usr/local/oraji/oraji.1 /usr/share/man/man1/oraji.1 &&
 echo "***success***" || echo "---Failed!!!---"
